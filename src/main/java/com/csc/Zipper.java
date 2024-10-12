037ab1725595b736fae61a8bcfec8fc4ebfdc024
Michael <michaelvargo@outlook.com>
2024-10-12 11:19:53 -0500

    Zipped

--- src/main/java/com/csc/Zipper.java
+++ src/main/java/com/csc/Zipper.java
@@ -1,5 +1,23 @@
 package com.csc;
 
-public class Zipper {
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+public class Zipper {
+    <T, U> List<Pair<T, U>> zip(List<T> list1, List<U> list2) {
+        return IntStream.range(0, Math.min(list1.size(), list2.size()))
+                .mapToObj(i -> new Pair<>(list1.get(i), list2.get(i)))
+                .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        Zipper zipper = new Zipper();
+        List<String> list1 = Arrays.asList("a", "b", "c");
+        List<String> list2 = Arrays.asList("one", "two", "three");
+
+        List<Pair<String, String>> zipped = zipper.zip(list1, list2);
+        zipped.forEach(pair -> System.out.println(pair.getFirst() + "," + pair.getSecond()));
+    }
 }
--- src/test/java/com/csc/ZipperTest.java
+++ src/test/java/com/csc/ZipperTest.java
@@ -1,13 +1,16 @@
 package com.csc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ZipperTest {
 
   Zipper zipper;
+  List<String> string1 = List.of("one", "three", "five");
+  List<String> string2 = List.of("two", "four", "six");
 
   @BeforeEach
   void setUp() {
@@ -16,6 +19,6 @@ public class ZipperTest {
 
   @Test
   void itWorks() {
-    assertEquals(true, true);
+    assertEquals("one,two,three,four,five,six,seven", zipper.zip(string1, string2));
   }
 }
